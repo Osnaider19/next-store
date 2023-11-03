1b80efeb4c3f31bbc04e91b76c9c536828f912ab
osnaider <osnaidermartinez2002@gmail.com>
2023-11-02 22:47:39 -0500

    validating the email

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,8 +3,8 @@ import { FromFooter } from "./FromFooter";
 
 export const Footer = () => {
   return (
-    <section className="w-full h-full bg-[#F5F7FF]">
-      <div className="flex justify-evenly items-center py-7">
+    <section className="w-full h-full bg-[#F5F7FF] pt-9">
+      <div className="flex justify-evenly items-center py-7 flex-wrap">
         <div className="flex justify-center items-center flex-col w-[270px]">
           <IconSupport />
           <h4 className="font-bold text-center py-3">Product Support</h4>
@@ -14,7 +14,9 @@ export const Footer = () => {
         </div>
 
         <div className="flex justify-center items-center flex-col w-[270px]">
-          <IconAccount />
+        <div className="w-[65px] h-[65px] rounded-full bg-[#0156FF]">
+          <IconAccount/>
+        </div>
           <h4 className="font-bold text-center py-3">Personal Account</h4>
           <p className="text-center">
             With big discounts, free delivery and a dedicated support
@@ -23,7 +25,9 @@ export const Footer = () => {
         </div>
 
         <div className="flex justify-center items-center flex-col w-[270px]">
+          <div className="w-[65px] h-[65px] rounded-full bg-[#0156FF]">
           <IconSaving />
+          </div>
           <h4 className="font-bold text-center py-3">Amazing Savings</h4>
           <p className="text-center">
             Up to 70% off new Products, you can be sure of the best price.
--- src/components/Footer/FromFooter.tsx
+++ src/components/Footer/FromFooter.tsx
@@ -1,6 +1,19 @@
-import React from "react";
+"use client"
+import React, { FormEvent } from "react";
 
 export const FromFooter = () => {
+  const handelFrom = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const { elements } = e.currentTarget
+    const email = elements.namedItem("email") as HTMLInputElement;
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    const isValidEmail = emailRegex.test(email.value);
+    if (isValidEmail) {
+      console.log("La dirección de correo electrónico es válida");
+    } else {
+      console.log("La dirección de correo electrónico no es válida");
+    }
+  };
   return (
     <div className="w-full h-full bg-[#000] text-white py-9">
       <div className="flex justify-between items-center w-[80%] m-auto">
@@ -10,10 +23,11 @@ export const FromFooter = () => {
             Be the first to hear about the latest offers.
           </p>
         </div>
-        <form>
+        <form onSubmit={handelFrom}>
           <div className="flex gap-x-2">
             <input
               type="email"
+              name="email"
               className="border border-white outline-none rounded-sm bg-[#000] px-4"
               placeholder="Your Email"
             />
