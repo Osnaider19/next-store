2e31189885430bc02697f06a3f280a5ef7e499c0
osnaider <osnaidermartinez2002@gmail.com>
2023-11-06 22:03:06 -0500

    adding comments to statuses

--- src/store/cartStore.ts
+++ src/store/cartStore.ts
@@ -1,66 +1,90 @@
 import { create } from "zustand";
 import { products } from "../data/products";
 import { type Products } from "@/types/types";
-import { persist , createJSONStorage , devtools} from "zustand/middleware";
+import { persist, createJSONStorage, devtools } from "zustand/middleware";
 interface CartStore {
   cart: Products[];
-  addToCart: (id: number) => void;
-  removeFromCart: (id: number) => void;
-  clearCart: () => void;
-  removeOneFromCart: (id: number) => void;
+  addToCart: (id: number) => void; // agregar productos al cart
+  removeFromCart: (id: number) => void; // remover un producto de el cart
+  clearCart: () => void; // limpiar el cart
+  removeOneFromCart: (id: number) => void; // remover la cantidad de el producto
 }
 
-export const useCartStore = create<CartStore>()(devtools(persist((set, get) => ({
-  cart: [],
-  addToCart: (id: number) => {
-    const newProduct = products.find((product) => id === product.id);
-    const productInCart = get().cart.find((product) => id === product.id);
+export const useCartStore = create<CartStore>()(
+  devtools(
+    persist(
+      (set, get) => ({
 
-    if (productInCart) {
-      const updatedCart = get().cart.map((product) =>
-        product.id === id
-          ? { ...product, amount: product.amount! + 1 }
-          : product
-      );
-      set({ cart: updatedCart });
-    } else {
-      if (newProduct) {
-        const product = { ...newProduct, amount: 1 };
-        set((state) => ({
-          ...state,
-          cart: [...state.cart, product],
-        }));
+        cart: [], //estado inicial de el cart
+
+        addToCart: (id: number) => {
+          const newProduct = products.find((product) => id === product.id); // buscamos en el arry de producto el que por id
+          const productInCart = get().cart.find((product) => id === product.id); // si el producto ya esta en el cart
+
+          //si el producto ya esta en el cart lo que vamos hacer es que a su propiedad amount le vamos a sumar lo que tenga la propiedad amount mas 1
+          if (productInCart) {
+            const updatedCart = get().cart.map((product) =>
+              product.id === id
+                ? { ...product, amount: product.amount! + 1 }
+                : product
+            );
+            set({ cart: updatedCart });
+            return; // retornamos para que no siga buscando
+          }
+          // si el producto no esta en el cart lo agregamos 
+          if (newProduct) {
+            const product = { ...newProduct, amount: 1 }; // mutamos el producto para que le agrege un propiedad amout que seria la cantidad
+
+            set((state) => ({ // actualizamos el estado con todo lo que tenga el estado mas el nuevo producto
+              ...state,
+              cart: [...state.cart, product],
+            }));
+            return;
+          }
+        },
+
+        removeFromCart: (id: number) => {
+          // buscamos entre todos los productos de el cart y filtarmos todos aquellos productos que se sean distintos con el id de el producto que fue enviado 
+          set((state) => ({
+            ...state,
+            cart: state.cart.filter((product) => product.id !== id),
+          }));
+        },
+
+        removeOneFromCart: (id: number) => {
+          // buscamos el producto a remover cantidad
+          const productToRemove = get().cart.find(
+            (product) => id === product.id
+          );
+          // si encuentra el producto hacemos un map de todos los productos de el  cart y si el id es igual al id de el producto que se esta recoriendo en ese momento a su propiedad amount le vamos a restar 1 
+          if (productToRemove) {
+            const updatedCart = get().cart.map((product) => {
+              if (product.id === id) {
+                return { ...product, amount: product.amount! - 1 };
+              }
+              return product;
+            });
+           
+            // actualizamos el estado con los productos actualizado mientras que en su propiedad amount de el producto se mayor a 0 
+            set((state) => ({
+              ...state,
+              cart: updatedCart.filter((product) => product.amount! > 0),
+            }));
+          }
+
+        },
+          
+        clearCart: () => {
+          // limpiar  el cart a su estado inicial
+          set({ cart: [] });
+        },
+
+      }),
+
+      {
+        name: "cart",
+        storage: createJSONStorage(() => localStorage), // guardar el estado en el localStorage
       }
-    }
-  },
-
-  removeFromCart: (id: number) => {
-    set((state) => ({
-      ...state,
-      cart: state.cart.filter((product) => product.id !== id),
-    }));
-  },
-  removeOneFromCart: (id: number) => {
-    const productToRemove = get().cart.find((product) => id === product.id);
-    if (productToRemove) {
-      const updatedCart = get().cart.map((product) => {
-        if (product.id === id) {
-          return { ...product, amount: product.amount! - 1 };
-        }
-        return product;
-      });
-
-      set((state) => ({
-        ...state,
-        cart: updatedCart.filter((product) => product.amount! > 0),
-      }));
-    }
-  },
-
-  clearCart: () => {
-    set({ cart: [] });
-  },
-}) , {
- name : "cart",
- storage: createJSONStorage(() => localStorage),
-})));
+    )
+  )
+);
--- src/store/productsStore.ts
+++ src/store/productsStore.ts
@@ -3,21 +3,22 @@ import { type Products } from "@/types/types";
 import { create } from "zustand";
 
 type ProductStore = {
-  products: Products[];
-  searchResults: Products[];
-  clearFilter: () => void;
+  products: Products[]; 
+  searchResults: Products[]; // aqui se almacenan los resultados de la busqueda
+  clearFilter: () => void; // limpiar los filtros 
   filters: (minprice: number, category: string) => void;
-  search: (value: string) => void;
+  search: (value: string) => void; // buscar por name o category
 };
 export const useProductsStore = create<ProductStore>((set, get) => ({
   products: products,
   searchResults: [],
-
   clearFilter: () => {
     set({ products: products });
   },
 
   filters: (minprice, category) => {
+
+    //si el la category es true y el minprice es true filtramos por los dos
     if (category && minprice) {
       const productsFound = products.filter(
         (product) => product.price > minprice && category === product.category
@@ -25,6 +26,7 @@ export const useProductsStore = create<ProductStore>((set, get) => ({
       set({ products: productsFound });
       return;
     }
+    // si solo la category es true filtramos los productos por category
     if (category && !minprice) {
       const productsFound = products.filter(
         (product) => category === product.category
@@ -33,6 +35,7 @@ export const useProductsStore = create<ProductStore>((set, get) => ({
       set({ products: productsFound });
       return;
     }
+    // si solo el price es true filtramos por el price
     if (minprice && !category) {
       const productsFound = products.filter(
         (product) => product.price > minprice
@@ -40,13 +43,16 @@ export const useProductsStore = create<ProductStore>((set, get) => ({
       set({ products: productsFound });
       return;
     }
+
   },
+
   search: (value) => {
-    // buscar los productos por nombre o por categoria 
-    const valueToCase = value.toLocaleLowerCase()
+    const valueToCase = value.toLocaleLowerCase() // transformamos el valor a buscar en minúsculas
+    //iteramos  el arry de productos que cumplan con la condicion de el filtro
     const productsFound = products.filter((item) =>
       item.title.toLocaleLowerCase().includes(valueToCase) || item.category.toLocaleLowerCase().includes(valueToCase)
     );
     set({ searchResults: productsFound });
   },
+
 }));
